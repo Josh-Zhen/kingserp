fbd16d9dd08949341360ba385f82e369cdd4bc72
by丶Moonlit/2 <by.Moonlit@gmail.com>
2020-06-19 17:50:35 +0800

    更新微信會員卡模塊

--- login/src/main/java/com/moonlit/kingserp/login/controller/common/CommonController.java
+++ login/src/main/java/com/moonlit/kingserp/login/controller/common/CommonController.java
@@ -1,14 +1,9 @@
 package com.moonlit.kingserp.login.controller.common;
 
-import com.moonlit.kingserp.common.response.ResponseObj;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @Description: 公共接口
@@ -22,17 +17,4 @@ import org.springframework.web.multipart.MultipartFile;
 @Api(value = "公共接口", tags = {"公共接口"})
 public class CommonController {
 
-    /**
-     * 微信圖片上傳接口
-     *
-     * @param file
-     * @return
-     */
-    @PostMapping("/WxUpdataImg")
-    @ApiOperation("微信圖片上傳接口")
-    public ResponseObj WxUpdataImg(@RequestParam(value = "file") MultipartFile file) {
-
-
-        return ResponseObj.createSuccessResponse();
-    }
 }
--- login/src/main/java/com/moonlit/kingserp/login/controller/create/WxCreateController.java
+++ login/src/main/java/com/moonlit/kingserp/login/controller/create/WxCreateController.java
@@ -1,8 +1,20 @@
 package com.moonlit.kingserp.login.controller.create;
 
+import com.alibaba.fastjson.JSON;
 import com.moonlit.kingserp.common.response.ResponseObj;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpCardService;
+import me.chanjar.weixin.mp.api.WxMpMemberCardService;
+import me.chanjar.weixin.mp.bean.card.MemberCard;
+import me.chanjar.weixin.mp.bean.card.MemberCardCreateRequest;
+import me.chanjar.weixin.mp.bean.card.WxMpCardCreateRequest;
+import me.chanjar.weixin.mp.bean.card.WxMpCardCreateResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,14 +30,47 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "微信会员卡", tags = {"微信会员卡"})
 public class WxCreateController {
 
+    @Autowired
+    private WxMpMemberCardService wxMpMemberCardService;
+
     /**
+     * 創建會員卡模板
+     *
      * @return
      */
-    public ResponseObj addCeateModel() {
+    @PostMapping("/addCeateModel")
+    @ApiOperation("創建會員卡模板")
+    public ResponseObj addCeateModel(@RequestBody MemberCard memberCard) throws WxErrorException {
+
+        // 設置自動開卡
+        memberCard.setWxActivate(true);
+        // 显示积分、支持储值
+        memberCard.setSupplyBonus(true);
+        memberCard.setSupplyBalance(true);
+        // 設置积分外链、余额外链(接口地址需要動態帶用戶id)
+        memberCard.setBonusUrl("");
+        memberCard.setBalanceUrl("");
+
+
+
+
+        MemberCardCreateRequest createRequest = new MemberCardCreateRequest();
+        createRequest.setMemberCard(memberCard);
+
+
+        // 轉String
+        String memberCardToString = JSON.toJSONString(createRequest);
+
+        //創建會員卡
+        WxMpCardCreateResult openCard = wxMpMemberCardService.createMemberCard(memberCardToString);
+        if (openCard.getErrmsg()=="ok"){
+            openCard.getCardId();
+
+        }
+
 
         return ResponseObj.createSuccessResponse();
     }
-
 
 
 }
