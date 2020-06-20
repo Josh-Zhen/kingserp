36afce77c10cf25eb7e007c35a6417bc8ae78fbe
by丶Moonlit/2 <by.Moonlit@gmail.com>
2020-06-20 12:14:15 +0800

    更新微信會員卡相關

--- entity/src/main/java/com/moonlit/kingserp/entity/login/WxMemberCard.java
+++ entity/src/main/java/com/moonlit/kingserp/entity/login/WxMemberCard.java
@@ -32,26 +32,31 @@ public class WxMemberCard implements Serializable {
     /**
      * 商户表主键id
      */
+    @ApiModelProperty(name = "商户表主键id", value = "merchantId")
     private Integer merchantId;
 
     /**
      * 商家自定义会员卡背景图
      */
+    @ApiModelProperty(name = "商家自定义会员卡背景图", value = "backgroundPicUrl")
     private String backgroundPicUrl;
 
     /**
      * 卡券的商户logo
      */
+    @ApiModelProperty(name = "卡券的商户logo", value = "logoUrl")
     private String logoUrl;
 
     /**
      * 商户名称（主标题，上限12个汉字）
      */
+    @ApiModelProperty(name = "商户名称（主标题，上限12个汉字）", value = "brandName")
     private String brandName;
 
     /**
      * 卡券名（副标题，上限9个汉字）
      */
+    @ApiModelProperty(name = "卡券名（副标题，上限9个汉字）", value = "title")
     private String title;
 
     /**
@@ -64,32 +69,38 @@ public class WxMemberCard implements Serializable {
      * "CODE_TYPE_NONE" 不显示任何码型）
      * 默认为：CODE_TYPE_QRCODE
      */
+    @ApiModelProperty(name = "Code展示类型", value = "codeType")
     private String codeType;
 
     /**
      * 卡券使用提醒，字数上限为16个汉字。
      */
+    @ApiModelProperty(name = "卡券使用提醒，字数上限为16个汉字。", value = "notice")
     private String notice;
 
     /**
      * 卡券使用说明，字数上限为1024个汉字。
      */
+    @ApiModelProperty(name = "卡券使用说明，字数上限为1024个汉字。", value = "description")
     private String description;
 
     /**
      * 客服电话
      */
+    @ApiModelProperty(name = "客服电话", value = "servicePhone")
     private String servicePhone;
 
     /**
      * 会员卡特权说明,限制1024汉字。
      */
+    @ApiModelProperty(name = "会员卡特权说明,限制1024汉字。", value = "prerogative")
     private String prerogative;
 
     /**
      * 商家服务类型：（可多选）
      * BIZ_SERVICE_DELIVER 外卖服务； BIZ_SERVICE_FREE_PARK 停车位； BIZ_SERVICE_WITH_PET 可带宠物； BIZ_SERVICE_FREE_WIFI 免费wifi
      */
+    @ApiModelProperty(name = "商家服务类型", value = "businessService")
     private String businessService;
 
     /**
@@ -102,11 +113,13 @@ public class WxMemberCard implements Serializable {
      * "k4":{"image_url":"圖片地址","text":"文案"}
      * }
      */
+    @ApiModelProperty(name = "图文列表，显示在详情内页 ，优惠券券开发者须至少传入 一组图文列表", value = "textImageList")
     private String textImageList;
 
     /**
      * 会员卡模板id
      */
+    @ApiModelProperty(name = "会员卡模板id", value = "cardId")
     private String cardId;
 
     /**
@@ -119,5 +132,12 @@ public class WxMemberCard implements Serializable {
      * "k4":{"name":"充值记录","tips":"","url":"地址"}
      * }
      */
-    private String custom_fields;
+    @ApiModelProperty(name = "自定義會員信息欄目", value = "customCells")
+    private String customCells;
+
+    /**
+     * 二維碼地址，需要調用二維碼轉圖片接口
+     */
+    @ApiModelProperty(name = "二維碼地址，需要調用二維碼轉圖片接口", value = "ticket")
+    private String ticket;
 }
--- login/src/main/java/com/moonlit/kingserp/login/controller/create/WxCreateController.java
+++ login/src/main/java/com/moonlit/kingserp/login/controller/create/WxCreateController.java
@@ -2,6 +2,7 @@ package com.moonlit.kingserp.login.controller.create;
 
 import com.alibaba.fastjson.JSON;
 import com.moonlit.kingserp.common.errer.ErrerMsg;
+import com.moonlit.kingserp.common.exception.EpException;
 import com.moonlit.kingserp.common.response.ResponseObj;
 import com.moonlit.kingserp.entity.login.WxMemberCard;
 import com.moonlit.kingserp.login.service.wx.WxMemberCardService;
@@ -10,9 +11,12 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpMemberCardService;
+import me.chanjar.weixin.mp.api.WxMpQrcodeService;
 import me.chanjar.weixin.mp.bean.card.MemberCard;
 import me.chanjar.weixin.mp.bean.card.MemberCardCreateRequest;
 import me.chanjar.weixin.mp.bean.card.WxMpCardCreateResult;
+import me.chanjar.weixin.mp.bean.membercard.WxMpMemberCardCreateMessage;
+import me.chanjar.weixin.mp.bean.result.WxMpQrCodeTicket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,14 +42,17 @@ public class WxCreateController {
 
     /**
      * 創建會員卡模板
+     * <p>
+     * Ps：當創建成功後，微信會審核，需等待3個工作日
      *
      * @param memberCard 會員卡實體
      * @return
-     * @throws WxErrorException
      */
     @PostMapping("/addCeateModel")
     @ApiOperation("創建會員卡模板")
-    public ResponseObj addCeateModel(@RequestBody MemberCard memberCard) throws WxErrorException {
+    public ResponseObj addCeateModel(@RequestBody MemberCard memberCard) {
+
+        Integer merchantId = 1;
 
         // 設置自動開卡
         memberCard.setWxActivate(true);
@@ -56,15 +63,22 @@ public class WxCreateController {
         memberCard.setBonusUrl("");
         memberCard.setBalanceUrl("");
 
+        // 封裝實體
         MemberCardCreateRequest createRequest = new MemberCardCreateRequest();
         createRequest.setMemberCard(memberCard);
-        String memberCardToString = JSON.toJSONString(createRequest);
+//        String memberCardToString = JSON.toJSONString(createRequest);
+        WxMpMemberCardCreateMessage wxMpMemberCardCreateMessage = new WxMpMemberCardCreateMessage();
+        wxMpMemberCardCreateMessage.setCardCreateRequest(createRequest);
 
         //創建會員卡
         try {
-            WxMpCardCreateResult openCard = wxMpMemberCardService.createMemberCard(memberCardToString);
-            if ("ok".equals(openCard.getErrmsg())) {
+//            WxMpCardCreateResult openCard = wxMpMemberCardService.createMemberCard(memberCardToString);
+            WxMpCardCreateResult openCard = wxMpMemberCardService.createMemberCard(wxMpMemberCardCreateMessage);
+
+            // 0為成功，其他皆爲錯誤
+            if (0 == openCard.getErrcode()) {
                 WxMemberCard wxMemberCard = new WxMemberCard();
+                wxMemberCard.setMerchantId(merchantId);
                 wxMemberCard.setBackgroundPicUrl(memberCard.getBackgroundPicUrl());
                 wxMemberCard.setBrandName(memberCard.getBaseInfo().getBrandName());
                 wxMemberCard.setLogoUrl(memberCard.getBaseInfo().getLogoUrl());
@@ -76,20 +90,22 @@ public class WxCreateController {
                 wxMemberCard.setPrerogative(memberCard.getPrerogative());
                 wxMemberCard.setBusinessService(memberCard.getAdvancedInfo().getBusinessServiceList().toString());
                 wxMemberCard.setTextImageList(memberCard.getAdvancedInfo().getTextImageList().toString());
-
+                // TODO 默認保存3個 需要多個後續使用json實行該功能
+                wxMemberCard.setCustomCells(memberCard.getCustomCell1().toString() + memberCard.getCustomCell2().toString() + memberCard.getCustomCell3().toString());
                 wxMemberCard.setCardId(openCard.getCardId());
+                // 存庫
                 if (0 > wxMemberCardService.insetr(wxMemberCard)) {
+                    //TODO 如果存庫失敗，需要提前保存已經設置的内容（待補充）
                     return ResponseObj.createErrResponse(ErrerMsg.ERRER20504);
                 }
             } else {
-
+                //TODO 如果返回異常
+                return ResponseObj.createErrResponse(ErrerMsg.ERRER10018);
             }
         } catch (WxErrorException e) {
             //TODO 補異常處理
-            e.getError();
+            throw new EpException("請求異常");
         }
-
-
         return ResponseObj.createSuccessResponse();
     }
 
